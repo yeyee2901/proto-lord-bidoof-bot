84146636ff0fc2b4a61750308f110ecc41f18897
Gabriel Sebastian <gabriel777sh@gmail.com>
2022-12-28 10:08:59 +0700

    add: send message rpc

--- gen/go/telegram/v1/telegram_service_grpc.pb.go
+++ gen/go/telegram/v1/telegram_service_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type TelegramServiceClient interface {
 	BotStatus(ctx context.Context, in *BotStatusRequest, opts ...grpc.CallOption) (*BotStatusResponse, error)
+	SendMessage(ctx context.Context, in *SendMessageRequest, opts ...grpc.CallOption) (*SendMessageResponse, error)
 }
 
 type telegramServiceClient struct {
@@ -42,11 +43,21 @@ func (c *telegramServiceClient) BotStatus(ctx context.Context, in *BotStatusRequ
 	return out, nil
 }
 
+func (c *telegramServiceClient) SendMessage(ctx context.Context, in *SendMessageRequest, opts ...grpc.CallOption) (*SendMessageResponse, error) {
+	out := new(SendMessageResponse)
+	err := c.cc.Invoke(ctx, "/telegram.v1.TelegramService/SendMessage", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // TelegramServiceServer is the server API for TelegramService service.
 // All implementations should embed UnimplementedTelegramServiceServer
 // for forward compatibility
 type TelegramServiceServer interface {
 	BotStatus(context.Context, *BotStatusRequest) (*BotStatusResponse, error)
+	SendMessage(context.Context, *SendMessageRequest) (*SendMessageResponse, error)
 }
 
 // UnimplementedTelegramServiceServer should be embedded to have forward compatible implementations.
@@ -55,6 +66,9 @@ type UnimplementedTelegramServiceServer struct {
 
 func (UnimplementedTelegramServiceServer) BotStatus(context.Context, *BotStatusRequest) (*BotStatusResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method BotStatus not implemented")
+}
+func (UnimplementedTelegramServiceServer) SendMessage(context.Context, *SendMessageRequest) (*SendMessageResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SendMessage not implemented")
 }
 
 // UnsafeTelegramServiceServer may be embedded to opt out of forward compatibility for this service.
@@ -86,6 +100,24 @@ func _TelegramService_BotStatus_Handler(srv interface{}, ctx context.Context, de
 	return interceptor(ctx, in, info, handler)
 }
 
+func _TelegramService_SendMessage_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SendMessageRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TelegramServiceServer).SendMessage(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/telegram.v1.TelegramService/SendMessage",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TelegramServiceServer).SendMessage(ctx, req.(*SendMessageRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // TelegramService_ServiceDesc is the grpc.ServiceDesc for TelegramService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -96,6 +128,10 @@ var TelegramService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "BotStatus",
 			Handler:    _TelegramService_BotStatus_Handler,
+		},
+		{
+			MethodName: "SendMessage",
+			Handler:    _TelegramService_SendMessage_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
